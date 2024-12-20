fb6a92eb5a5a3b905c7297dff56edf73e8663df9
Daniel <123036824+danroke@users.noreply.github.com>
2024-12-20 20:14:07 +0000

    regex solution

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-# start
+# regex module
+import re
+
+Total_sum = 0
+matches_all = []
+
+with open('input.txt', 'r') as file:
+    for line in file:    
+
+        pattern = r"mul\(\d{1,3},\d{1,3}\)"
+        matches = re.findall(pattern, line)
+        
+        for match in matches:
+            match_string = re.split(r",",match)
+            x = re.split(r"\(",match_string[0])
+            y = re.split(r"\)", match_string[1])
+            result = int(x[1]) * int(y[0])
+            Total_sum = Total_sum + result
+        
+    print("Total Sum:", Total_sum)
